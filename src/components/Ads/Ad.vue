a8203c8b6e6b2a68e05c674040c1f609081eb912
Alexey-aka-sultan <alex_240981@mail.ru>
2019-11-09 18:53:58 +0600

    create ad page

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,18 @@
   <v-container>
       <v-layout row>
           <v-flex xs12>
-              <h1>Ad</h1>
+              <v-card>
+                  <v-img src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg" height="300"></v-img>
+                  <v-card-text>
+                      <h1 class="text--primary">lorem</h1>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, ex placeat vitae numquam</p>
+                  </v-card-text>
+                  <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn class="warning" text>Edit</v-btn>
+                      <v-btn color="success">Buy</v-btn>
+                  </v-card-actions>
+              </v-card>
           </v-flex>
       </v-layout>
   </v-container>
