f2bb17b86a7ec7c3e4d10767eb0e97746c04de17
Alexey-aka-sultan <alex_240981@mail.ru>
2019-11-09 12:17:19 +0600

    create newAd page

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,21 +1,85 @@
 <template>
   <v-container>
-      <v-layout row>
+    <v-layout row>
+      <v-flex xs12 sm-6 offset-sm-3>
+        <h1 class="text--secondary mb-3">Create new ad</h1>
+
+        <v-form ref="form" v-model="valid" lazy-validation class="mb-3">
+          <v-text-field
+            label="Ad title"
+            name="title"
+            type="text"
+            required
+            :rules="[v=>!!v||'Title is required']"
+            v-model="title"
+          />
+
+          <v-textarea
+            label="Ad description"
+            name="description"
+            type="text"
+            :rules="[v=>!!v||'Description is required']"
+            v-model="description"
+          />
+        </v-form>
+
+        <v-layout row mb-3>
           <v-flex xs12>
-              <h1>NewAd</h1>
+            <v-btn color="blue-grey" class="ma-2 white--text">
+              Upload
+              <v-icon right dark>mdi-cloud-upload</v-icon>
+            </v-btn>
           </v-flex>
-      </v-layout>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex xs12>
+            <img src height="100" />
+          </v-flex>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex xs12>
+            <v-switch v-model="promo" :label="'Add to promo?'"></v-switch>
+          </v-flex>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex xs12>
+            <v-spacer></v-spacer>
+            <v-btn :disabled="!valid" class="success" @click="createAd">Create ad</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 export default {
-    data: () => ({
-
-    })
-}
+  data() {
+    return {
+      title: "",
+      description: "",
+      promo: false,
+      valid: false
+    };
+  },
+  methods: {
+    createAd() {
+      if (this.$refs.form.validate()) {
+        const ad = {
+          title: this.title,
+          description: this.description,
+          promo: this.promo
+        };
+        console.log(ad);
+        
+      }
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
