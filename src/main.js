e6290af463d4effe918d3c73febace78f9ca8b1a
Alexey-aka-sultan <alex_240981@mail.ru>
2019-11-10 23:01:06 +0600

    fx

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './routes/index.js'
-import store from './store/index.js'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./routes/index.js";
+import store from "./store/index.js";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   router,
   store,
+  created() {
+    // var firebaseConfig = {
+    //   apiKey: "AIzaSyAc7vZXdxPsyHFezZTA39vQAOi0HXgR4Dk",
+    //   authDomain: "itc-ads-3f211.firebaseapp.com",
+    //   databaseURL: "https://itc-ads-3f211.firebaseio.com",
+    //   projectId: "itc-ads-3f211",
+    //   storageBucket: "itc-ads-3f211.appspot.com",
+    //   messagingSenderId: "585806385925",
+    //   appId: "1:585806385925:web:36dd811f0ab5b788719d6c"
+    // };
+  },
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
---
+++ src/store/user.js
@@ -0,0 +1,12 @@
+export default {
+  state: {
+    user: null
+  },
+  mutations: {},
+  actions: {},
+  getters: {
+    user(state) {
+      return state.user;
+    }
+  }
+};
