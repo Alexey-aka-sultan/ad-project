81017e87197a8110903b8bdcae988406bd7163dd
Alexey-aka-sultan <alex_240981@mail.ru>
2019-11-29 18:18:37 +0600

    lesson-27

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,14 +1,27 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12 sm-6 offset-sm3>
+      <v-row xs12 align="center" justify="center" v-if="loading">
+        <v-progress-circular
+          :size="100"
+          color="purple"
+          indeterminate
+        ></v-progress-circular>
+      </v-row>
+
+      <v-flex v-else-if="!loading && orders.length !== 0" xs12 sm-6 offset-sm3>
         <h1 class="text--secondary mb-3">Orders</h1>
         <v-list subheader two-line flat max-width="475">
           <v-list-item-group>
             <v-list-item v-for="(order, i) in orders" :key="i">
               <template>
                 <v-list-item-action>
-                  <v-checkbox :ref="'check'+order.adId" @change="markDone(order, $event)" :input-value="order.done" color="success"></v-checkbox>
+                  <v-checkbox
+                    :ref="'check' + order.adId"
+                    @change="markDone(order, $event)"
+                    :input-value="order.done"
+                    color="success"
+                  ></v-checkbox>
                 </v-list-item-action>
 
                 <v-list-item-content>
@@ -17,12 +30,16 @@
                 </v-list-item-content>
 
                 <v-list-item-action>
-                  <v-btn :to="'/ad/'+order.adId" class="primary">Open</v-btn>
+                  <v-btn :to="'/ad/' + order.adId" class="primary">Open</v-btn>
                 </v-list-item-action>
               </template>
             </v-list-item>
           </v-list-item-group>
         </v-list>
+      </v-flex>
+
+      <v-flex v-else>
+        <h1 class="text--secondary">You have no orders</h1>
       </v-flex>
     </v-layout>
   </v-container>
@@ -30,26 +47,30 @@
 
 <script>
 export default {
-  data() {
-    return {
-      orders: [
-        {
-          id: "",
-          name: "Samuel",
-          phone: "8-555-658-4598",
-          adId: "1",
-          done: false
-        }
-      ]
-    };
-  },
   methods: {
-    markDone(order, e) {
-      order.done = e;      
+    markDone(order) {
+      console.log(order);
+      
+      this.$store
+        .dispatch("markOrderDone", order.id)
+        .then(() => {
+          order.done = true;
+        })
+        .catch(() => {});
     }
+  },
+  computed: {
+    loading() {
+      return this.$store.getters.loading;
+    },
+    orders() {
+      return this.$store.getters.orders;
+    }
+  },
+  created() {
+    this.$store.dispatch("fetchOrders");
   }
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- src/store/orders.js
+++ src/store/orders.js
@@ -1,23 +1,86 @@
 /*eslint-disable */
 
+class Order {
+  constructor(name, phone, adId, done = false, id = null) {
+    this.name = name;
+    this.phone = phone;
+    this.adId = adId;
+    this.done = done;
+    this.id = id;
+  }
+}
+
 export default {
   state: {
     orders: []
   },
   mutations: {
-    createOrder(state, payload) {
-      state.orders.push(payload);
+    loadOrders(state, payload) {
+      state.orders = payload;
     }
   },
   actions: {
-    async createOrder() {
-      await new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve();
-        }, 4000);
-      });
+    async createOrder({ commit }, { name, phone, adId, ownerId }) {
+      const order = new Order(name, phone, adId);
+      commit("clearError");
+
+      try {
+        await firebase
+          .database()
+          .ref(`users/${ownerId}/orders`)
+          .push(order);
+      } catch (error) {
+        commit("setError", error.message);
+        throw error;
+      }
+    },
+    async fetchOrders({ commit, getters }) {
+      commit("clearError");
+      commit("setLoading", true);
+
+      const resultOrders = [];
+
+      try {
+        const fbVal = await firebase
+          .database()
+          .ref(`users/${getters.user.id}/orders`)
+          .once("value");
+        const orders = fbVal.val();
+
+        Object.keys(orders).forEach(key => {
+          const o = orders[key];
+          resultOrders.push(new Order(o.name, o.phone, o.adId, key));
+        });
+
+        commit("loadOrders", resultOrders);
+        commit("setLoading", false);
+      } catch (error) {
+        commit("setError", error.message);
+        commit("setLoading", false);
+        throw error;
+      }
+    },
+    async markOrderDone({ commit, getters }, payload) {
+      commit("clearError");
+      try {
+        await firebase
+          .database()
+          .ref(`users/${getters.user.id}/orders`)
+          .child(payload)
+          .update({ done: true });
+      } catch (error) {}
     }
   },
-  getters: {}
+  getters: {
+    doneOrders(state) {
+      return state.orders.filter(o => o.done);
+    },
+    undoneOrders(state) {
+      return state.orders.filter(o => !o.done);
+    },
+    orders(state, getters) {
+      return getters.undoneOrders.concat(getters.doneOrders);
+    }
+  }
 };
 /*eslint-enable */
